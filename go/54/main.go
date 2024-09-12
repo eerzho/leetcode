f288fbe86051ea51b19d36e040de1e44e9cd40a0
zhanbolat <z.yerkinbay@slotegrator.space>
2024-09-12 09:07:35 +0200

    add 54

--- go/54/main.go
+++ go/54/main.go
@@ -13,10 +13,48 @@ func spiralOrder(matrix [][]int) []int {
 		return []int{}
 	}
 
-	f := matrix[0][0]
 	result := []int{}
+	left, right, top, bottom := 0, m-1, 0, n-1
 
-	return append([]int{f}, result...)
+	for {
+		// ➡️
+		for i := left; i <= right; i++ {
+			result = append(result, matrix[top][i])
+		}
+		top++
+		if top > bottom {
+			break
+		}
+
+		// ⬇️
+		for i := top; i <= bottom; i++ {
+			result = append(result, matrix[i][right])
+		}
+		right--
+		if left > right {
+			break
+		}
+
+		// ⬅️
+		for i := right; i >= left; i-- {
+			result = append(result, matrix[bottom][i])
+		}
+		bottom--
+		if top > bottom {
+			break
+		}
+
+		// ⬆️
+		for i := bottom; i >= top; i-- {
+			result = append(result, matrix[i][left])
+		}
+		left++
+		if left > right {
+			break
+		}
+	}
+
+	return result
 }
 
 func main() {
